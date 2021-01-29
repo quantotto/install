2833d4556406117590ed74eae5f4ae490881ea9d
Yevgeny Menaker <jeka.menaker@gmail.com>
2021-01-28 20:46:04 -0800

    use namespace

--- server/install_server.sh
+++ server/install_server.sh
@@ -89,9 +89,9 @@ if [ -z $1 ] || [ -z $2 ]; then
 else
     echo "CUSTOMER_ID=$1" > $QUANTOTTO_HOME/.env
     echo "FRAMES_PORT=15000" >> $QUANTOTTO_HOME/.env
-    echo "HYDRA_CUSTOMER_CLIENT_SECRET=$(kubectl get secret/customer-secret-$1 -n quantotto --template={{.data.HYDRA_CLIENT_SECRET}} | base64 --decode)" >> $QUANTOTTO_HOME/.env
-    echo "SERVER_IP=$(kubectl get service nginx -n quantotto | grep nginx | awk '{ printf($4); }')" >> $QUANTOTTO_HOME/.env
-    echo "PORTAL_FQDN=$(kubectl get configmap/api-common -n quantotto --template={{.data.PORTAL_FQDN}})" >> $QUANTOTTO_HOME/.env
+    echo "HYDRA_CUSTOMER_CLIENT_SECRET=$(kubectl get secret/customer-secret-$1 -n $2 --template={{.data.HYDRA_CLIENT_SECRET}} | base64 --decode)" >> $QUANTOTTO_HOME/.env
+    echo "SERVER_IP=$(kubectl get service nginx -n $2 | grep nginx | awk '{ printf($4); }')" >> $QUANTOTTO_HOME/.env
+    echo "PORTAL_FQDN=$(kubectl get configmap/api-common -n $2 --template={{.data.PORTAL_FQDN}})" >> $QUANTOTTO_HOME/.env
 
     mkdir -p $QUANTOTTO_HOME/certs
     kubectl get secret/certs -n quantotto -o 'go-template={{index .data "tls.crt"}}' | base64 --decode > $QUANTOTTO_HOME/certs/quantotto.crt
